0a3a1199ba795b935455c56389bc3e0bce2c6abc
Dan Acceleanu <acceleanu@gmail.com>
2019-05-22 13:29:52 +0100

    refactor tests

--- src/test/java/com/deltapunkt/start/LiveOrderBoardTest.java
+++ src/test/java/com/deltapunkt/start/LiveOrderBoardTest.java
@@ -1,5 +1,6 @@
 package com.deltapunkt.start;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import java.util.List;
@@ -9,17 +10,18 @@ import static com.deltapunkt.start.Order.createOrder;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class LiveOrderBoardTest {
+    private LiveOrderBoard unit;
+
+    @Before
+    public void setUp() {
+        // Assume that the values for quantity and price were already transformed in the given units
+        // and the units are the same for the whole board
+        unit = new LiveOrderBoard();
+    }
+
     @Test
     public void registerOneOrder() {
-        // Also assume that the values for quantity and price were already transformed in the given units
-        // and the units are the same for the whole board
-        LiveOrderBoard unit = new LiveOrderBoard();
-        Order order = createOrder( // create order with unique id
-            "userId",
-            "3.5", // use BigDecimal internally to represent the quantity
-            30301, // price modelled as cents in the given currency - so here we have £303
-            SELL         // use an enum to model the order type
-        );
+        Order order = createOrder1();
 
         List<Order> orders = unit.registerOrder(order); // make this return existing orders
 
@@ -28,18 +30,21 @@ public class LiveOrderBoardTest {
 
     @Test
     public void cancelExistingOrder() {
-        LiveOrderBoard unit = new LiveOrderBoard();
-        Order order = createOrder(
-            "userId",
-            "3.5",
-            30301,
-            SELL
-        );
+        Order order = createOrder1();
 
         List<Order> orders1 = unit.registerOrder(order);
         assertThat(orders1).containsExactly(order);
 
         List<Order> orders2 = unit.cancelOrder(order.getId());
         assertThat(orders2).isEmpty();
+    }
+
+    private Order createOrder1() {
+        return createOrder( // create order with unique id
+            "userId",
+            "3.5", // use BigDecimal internally to represent the quantity
+            30301, // price modelled as cents in the given currency - so here we have £303
+            SELL         // use an enum to model the order type
+        );
     }
 }
